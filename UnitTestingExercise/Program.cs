d69996e068e17fb41a30aa2260f7fe80128bf29f
jared mckemy <jaredmkcemy4@gmail.com>
2023-09-18 09:36:39 -0700

    made changes

--- UnitTestingExercise/Program.cs
+++ UnitTestingExercise/Program.cs
@@ -6,7 +6,16 @@ namespace UnitTestingExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calculator calculator = new Calculator();
+
+            int difference = calculator.Subtract(10, 5);
+            Console.WriteLine($"Subtraction Result: {difference}");
+
+            int product = calculator.Multiply(2, 3);
+            Console.WriteLine($"Multiplication Result: {product}");
+
+            double quotient = calculator.Divide(10, 2);
+            Console.WriteLine($"Division Result: {quotient}");
         }
     }
 }
